069745edce26cf0bd0f64a176008abcaa233c71c
kmj1117 <kmj1117@g.skku.edu>
2022-02-26 20:58:07 +0900

    MainPage 만들기

---
+++ GiSungIn/client/src/components/mainpage/Header.js
@@ -0,0 +1,44 @@
+import { Button, Container, Link, Toolbar } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const sections = [
+    {title : "GiSungIn", url : '#'},
+    {title : "소개", url : '#'},
+    {title : "서비스", url : '#'},
+    {title : "다운로드", url : '#'},
+];
+
+function Header(){
+    const navigate = useNavigate();
+    const onLogInClick = () => {
+        navigate("/login");
+    };
+    return(
+        <Container disableGutters maxWidth = "1g" >
+            <Toolbar 
+                component = "nav"
+                variant = "dense"
+                sx = {{justifyContent : 'space-between', overflowX:'auto'}}>
+                    
+                {sections.map((section) => (
+                    <Link
+                        color="inherit"
+                        noWrap
+                        key={section.title}
+                        variant="body2"
+                        href={section.url}
+                        sx={{ p: 1, flexShrink: 0 }}
+                    >
+                        {section.title}
+                    </Link>
+                ))}
+                <Button onClick={onLogInClick} variant="contained" color="primary" size="small">
+                    로그인
+                </Button>
+            </Toolbar>
+        </Container>
+    );
+}
+
+export default Header;
