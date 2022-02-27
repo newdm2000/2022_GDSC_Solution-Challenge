4c5f1ec3c68b95ee478d605733695d65a3e77509
Dongmin Shin <newdm2000@gmail.com>
2022-02-27 23:03:40 +0900

    2022-02-27

--- GiSungIn/client/src/components/body/BLectureCard.js
+++ GiSungIn/client/src/components/body/BLectureCard.js
@@ -1,26 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDoc, query, where, doc } from "firebase/firestore";
 import { dbService } from "fbase";
 import { Box, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const BLectureCard = ({ lectureId }) => {
   const navigate = useNavigate();
-  useEffect(() => {
-    refresh();
-  }, []);
+  useEffect(async () => {
+    if (lectureId) {
+      const docRef = doc(dbService, "Lectures", lectureId);
+      const docSnap = await getDoc(docRef);
+      setLectureObj(docSnap.data());
+    }
+  }, [lectureId]);
   const [lectureObj, setLectureObj] = useState("");
-
-  const refresh = async () => {
-    const lectureQuery = query(
-      collection(dbService, "Lectures"),
-      where("lid", "==", lectureId)
-    );
-    const querySnapShot = await getDocs(lectureQuery);
-    querySnapShot.forEach((doc) => {
-      setLectureObj(doc.data());
-    });
-  };
   const tempStyle = {
     display: "flex",
     flexDirection: "column",
@@ -31,7 +24,6 @@ const BLectureCard = ({ lectureId }) => {
     marginBottom: "20px",
     width: "100%",
   };
-
   return (
     <div className="container" style={tempStyle}>
       {/* <Box onClick = {()=>{console.log({lectureId})}} */}
@@ -65,9 +57,9 @@ const BLectureCard = ({ lectureId }) => {
             {/* <img src={lectureObj.img_url} style = {{border : 'solid red', position : 'absolute', width:'auto', height : 'auto', maxHeight : '100%', margin : '0px auto', WebkitTransform : 'translate(50%,50%)', MsTransform : 'translate(50%,50%)',transform : 'translate(50%,50%)'}} alt = 'loading' /> */}
           </Grid>
           <Grid item>
-            <h4>{lectureObj.name}</h4>
-            {lectureObj.category &&
-              lectureObj.category.map((field, index) => (
+            <h4>{lectureObj.lecName}</h4>
+            {lectureObj.lecField &&
+              lectureObj.lecField.map((field, index) => (
                 <div key={index}>
                   <h5>#{field} </h5>
                 </div>
--- GiSungIn/client/src/components/body/Bsearch.js
+++ GiSungIn/client/src/components/body/Bsearch.js
@@ -1,20 +1,23 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import LectureCard from "components/LectureCard";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { authService, dbService } from "fbase";
 import {
   collection,
   getDocs,
+  doc,
   getFirestore,
   query,
   where,
+  updateDoc,
 } from "firebase/firestore";
 
 function Bsearch() {
   const leclist = [];
   const [lid, setLid] = useState("");
   const { key } = useParams();
+  const navigate = useNavigate();
   useEffect(async () => {
     const user = auth.currentUser;
     const userQuery = query(
@@ -25,7 +28,6 @@ function Bsearch() {
     querySnapShot.forEach((doc) => {
       setUserObj(doc.data());
     });
-    console.log(userObj);
 
     const lecturesQuery = await getDocs(collection(dbService, "Lectures"));
     await lecturesQuery.forEach((doc) => {
@@ -36,14 +38,29 @@ function Bsearch() {
     console.log(randlec);
   }, []);
 
+  const onclickleft = () => {
+    console.log(key);
+  };
+  const onclickright = async () => {
+    const lec = userObj.lectures;
+    lec.push(lid);
+    const user = doc(dbService, "Users", userObj.uid);
+    await updateDoc(user, { lectures: lec });
+  };
+
   const auth = authService;
   const [userObj, setUserObj] = useState("");
 
   return (
     <Grid container>
-        <Grid item></Grid>
+      <Grid item>
+        <Button onClick={onclickleft}>left</Button>
+      </Grid>
       <Grid item>
         <LectureCard lectureId={lid} />
+      </Grid>
+      <Grid item>
+        <Button onClick={onclickright}>right</Button>
       </Grid>
     </Grid>
   );
