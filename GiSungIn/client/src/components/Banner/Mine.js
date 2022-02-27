f2f46acbf5277d68a0e6135c3e02f020fa7b552c
kmj1117 <kmj1117@g.skku.edu>
2022-02-27 19:02:05 +0900

    lectureCard

--- GiSungIn/client/src/components/Banner/Mine.js
+++ GiSungIn/client/src/components/Banner/Mine.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import BLectureCard from "./BLectureCard";
+import { dbService } from "fbase";
 
-function Mine() {
+const Mine = () => {
+  useEffect(() => {
+    refresh();
+  }, []);
+
+  const auth = getAuth();
+  const [userObj, setUserObj] = useState("");
+
+  const refresh = async () => {
+    const user = auth.currentUser;
+    const userQuery = query(
+      collection(dbService, "Users"),
+      where("uid", "==", user.uid)
+    );
+    const querySnapShot = await getDocs(userQuery);
+    querySnapShot.forEach((doc) => {
+      setUserObj(doc.data());
+    });
+    console.log(userObj.lectures);
+  };
+
   return (
-    <div>Mine</div>
+    <> 
+      {
+        userObj && (
+          userObj.lectures.length === 0 ? 
+            <h4>탐색을 먼저 해주세요</h4>
+          :
+          userObj.lectures.map((lid, index) => (
+            <div key = {index}>
+              <BLectureCard lectureId={lid} />
+            </div>
+          ))
+        )
+      }
+    </>
   )
 }
 
