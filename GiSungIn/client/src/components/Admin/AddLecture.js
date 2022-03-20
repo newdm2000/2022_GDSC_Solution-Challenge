b27219a3a29f7776cccd14bd1c9149eeb5c03074
dongmin Shin <newdm2000@gmail.com>
2022-03-20 20:06:40 +0900

    addlecture

--- GiSungIn/client/src/components/Admin/AddLecture.js
+++ GiSungIn/client/src/components/Admin/AddLecture.js
@@ -4,26 +4,31 @@ import { addDoc, doc, collection } from "firebase/firestore";
 import { dbService, storageService } from "fbase";
 import { category_list } from "data";
 import { useNavigate } from "react-router-dom";
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import ClearRoundedIcon from "@mui/icons-material/ClearRounded";
 import { getDownloadURL, ref, uploadString } from "firebase/storage";
-
 
 function AddLecture() {
   const [lectureOrder, setLectureOrder] = useState([]);
   const [aLecture, setALecture] = useState("");
   const [lecBrand, setLecBrand] = useState("");
   //const [lecBrandImg, setLecBrandImg] = useState("");
-  const [attachmentBrandImgUrl, setAttachmentBrandImgUrl]= useState("");
-  const [lecDescription, setLecDescription] = useState("")
+  const [attachmentBrandImgUrl, setAttachmentBrandImgUrl] = useState("");
+  const [lecDescription, setLecDescription] = useState("");
   const [lecName, setLecName] = useState("");
-  //const [lecImg, setLecImg] = useState("");
+  const [lecTime, setLecTime] = useState("");
+  const [lecMethod, setLecMethod] = useState("");
   const [attachmentImgUrl, setAttachmentImgUrl] = useState("");
+  const [lecPeriod, setLecPeriod] = useState("");
+  const [lecLanguage, setLecLanguage] = useState("");
   const [lecPrice, setLecPrice] = useState(0);
   const [lecField, setLecField] = useState([]);
+  const [lecIndex, setLecIndex] = useState([]);
+  const [index, setIndex] = useState("");
   const [lecUrl, setLecUrl] = useState("");
   const [num, setNum] = useState(0);
   const [cnt, setCnt] = useState(0);
+
   const navigate = useNavigate();
   const onChange = (event) => {
     const {
@@ -37,29 +42,49 @@ function AddLecture() {
       setLecPrice(value);
     } else if (name === "num") {
       setNum(value);
-    } /* else if (name === "lecImg") {
-      setLecImg(value);
-    } */ else if (name === "lecUrl") {
+    } else if (name === "index") {
+      setIndex(value);
+    } else if (name === "lecUrl") {
       setLecUrl(value);
+    } else if (name === "lecPeriod") {
+      setLecPeriod(value);
+    } else if (name === "lecLanguage") {
+      setLecLanguage(value);
+    } else if (name === "lecTime") {
+      setLecTime(value);
+    } else if (name === "lecMethod") {
+      setLecMethod(value);
     }
   };
 
   const onSubmit = async (event) => {
     event.preventDefault();
     let lecImg = "";
-    if(attachmentImgUrl !== ""){
+    if (attachmentImgUrl !== "") {
       const lecImgRef = ref(storageService, `Lectures/lecImg/${lecName}`);
-      const responseImg = await uploadString(lecImgRef, attachmentImgUrl, "data_url");
+      const responseImg = await uploadString(
+        lecImgRef,
+        attachmentImgUrl,
+        "data_url"
+      );
       lecImg = await getDownloadURL(responseImg.ref);
     }
     let lecBrandImg = "";
-    if(attachmentBrandImgUrl !== ""){
-      const lecBrandImgRef = ref(storageService, `Lectures/lecBrandImg/${lecBrand}`);
-      const responseBrandImg = await uploadString(lecBrandImgRef, attachmentBrandImgUrl, "data_url"); 
+    if (attachmentBrandImgUrl !== "") {
+      const lecBrandImgRef = ref(
+        storageService,
+        `Lectures/lecBrandImg/${lecBrand}`
+      );
+      const responseBrandImg = await uploadString(
+        lecBrandImgRef,
+        attachmentBrandImgUrl,
+        "data_url"
+      );
       lecBrandImg = await getDownloadURL(responseBrandImg.ref);
-    }
-    else{
-      lecBrandImg = await getDownloadURL(ref(storageService, `Lectures/lecBrandImg/${lecBrand}`));
+    } else {
+      lecBrandImg = await getDownloadURL(
+        ref(storageService, `Lectures/lecBrandImg/${lecBrand}`)
+      );
     }
     await addDoc(collection(dbService, "Lectures"), {
       lecBrand: lecBrand,
@@ -69,6 +94,11 @@ function AddLecture() {
       lecUrl: lecUrl,
       lecPrice: lecPrice,
       lecField: lecField,
+      lecIndex: lecIndex,
+      lecMethod: lecMethod,
+      lecLanguage: lecLanguage,
+      lecTime: lecTime,
+      lecPeriod: lecPeriod,
     });
     alert("Success");
     navigate("/");
@@ -94,47 +124,70 @@ function AddLecture() {
     setCnt(cnt + 1);
     console.log(lecField);
   };
-  const onBrandImgChange = (event) =>{
-    const {target : {files},
+  const onBrandImgChange = (event) => {
+    const {
+      target: { files },
     } = event;
-     const theFile = files[0];
-     const reader = new FileReader();
-     reader.onloadend = (finishedEvent) =>{
-        const {
-            currentTarget : {result},
-        } = finishedEvent;
-        setAttachmentBrandImgUrl(result);
-     }
-     reader.readAsDataURL(theFile);
-}
-  const onImgChange = (event) =>{
-    const {target : {files},
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachmentBrandImgUrl(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onImgChange = (event) => {
+    const {
+      target: { files },
     } = event;
-     const theFile = files[0];
-     const reader = new FileReader();
-     reader.onloadend = (finishedEvent) =>{
-        const {
-            currentTarget : {result},
-        } = finishedEvent;
-        setAttachmentImgUrl(result);
-     }
-     reader.readAsDataURL(theFile);
-}
-const onClearImg = () => {setAttachmentImgUrl("")};
-const onClearBrandImg = () => {setAttachmentBrandImgUrl("")};
-const AddLec = () =>{
-  <TextField
-    margin="normal"
-    type="text"
-    required
-    fullWidth
-    autoFocus
-    onChange={setALecture(aLecture)}
-  />
-  lectureOrder.push(aLecture);
-}
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachmentImgUrl(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearImg = () => {
+    setAttachmentImgUrl("");
+  };
+  const onClearBrandImg = () => {
+    setAttachmentBrandImgUrl("");
+  };
+  const onAddIndex = () => {
+    const lindex = lecIndex;
+    lindex.push(index);
+    setLecIndex(lindex);
+    setIndex("");
+    setCnt(cnt + 1);
+  };
+
+  const onDeleteIndex = () => {
+    const lindex = lecIndex;
+    lindex.pop();
+    setLecIndex(lindex);
+    setIndex("");
+    setCnt(cnt + 1);
+  };
+
+  const AddLec = () => {
+    <TextField
+      margin="normal"
+      type="text"
+      required
+      fullWidth
+      autoFocus
+      onChange={setALecture(aLecture)}
+    />;
+    lectureOrder.push(aLecture);
+  };
+
   return (
-    <Box component="form" noValidate sx={{ml:1, mt: 1, width:0.7}}>
+    <Box component="form" noValidate sx={{ ml: 1, mt: 1, width: 0.7 }}>
       <TextField
         margin="normal"
         type="text"
@@ -148,37 +201,41 @@ const AddLec = () =>{
         onChange={onChange}
       />
       <Button variant="contained">
-        <input 
-        id = "attach-file"
-        type = "file"
-        accept="image/*"
-        onChange = {onBrandImgChange}
-        style = {{
-          opacity : 0,
-        }}/>
-        <label htmlFor = "attach-file">
+        <input
+          id="attach-file"
+          type="file"
+          accept="image/*"
+          onChange={onBrandImgChange}
+          style={{
+            opacity: 0,
+          }}
+        />
+        <label htmlFor="attach-file">
           <span>Brand Img</span>
           <AddRoundedIcon />
         </label>
       </Button>
-      <Box 
-      sx = {{display : "flex",
-            justifyContent: "flex-start",
-            height : "100px",
-            weight : "100px",
-            color : "#000000",}}
-            >
-            <img 
-            src = {attachmentBrandImgUrl} 
-            alt = "no image"
-            style = {{
-                backgroundImage : "#000000",
-                height : "100px",
-                weight : "100px",
-            }}/>
-          <IconButton onClick = {onClearBrandImg}>
-              <ClearRoundedIcon />
-          </IconButton>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          height: "100px",
+          weight: "100px",
+          color: "#000000",
+        }}
+      >
+        <img
+          src={attachmentBrandImgUrl}
+          alt="no image"
+          style={{
+            backgroundImage: "#000000",
+            height: "100px",
+            weight: "100px",
+          }}
+        />
+        <IconButton onClick={onClearBrandImg}>
+          <ClearRoundedIcon />
+        </IconButton>
       </Box>
       <TextField
         margin="normal"
@@ -194,53 +251,70 @@ const AddLec = () =>{
       />
 
       <Button variant="contained">
-        <input 
-        id = "attach-file"
-        type = "file"
-        accept="image/*"
-        onChange = {onImgChange}
-        style = {{
-          opacity : 0,
-        }}/>
-        <label htmlFor = "attach-file">
-            <span>Lecture Img</span>
-            <AddRoundedIcon />
+        <input
+          id="attach-file"
+          type="file"
+          accept="image/*"
+          onChange={onImgChange}
+          style={{
+            opacity: 0,
+          }}
+        />
+        <label htmlFor="attach-file">
+          <span>Lecture Img</span>
+          <AddRoundedIcon />
         </label>
       </Button>
 
-      <Box 
-      sx = {{display : "flex",
-            justifyContent: "flex-start",
-            height : "100px",
-            weight : "100px",
-            color : "#000000",
-            backgroundImage : "#000000",}}
-            >
-            <img 
-            src = {attachmentImgUrl} 
-            alt = "no image"
-            style = {{
-              backgroundImage : "#000000",
-              height : "100px",
-                weight : "100px",
-            }}/>
-          <IconButton onClick = {onClearImg}>
-              <ClearRoundedIcon />
-          </IconButton>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          height: "100px",
+          weight: "100px",
+          color: "#000000",
+          backgroundImage: "#000000",
+        }}
+      >
+        <img
+          src={attachmentImgUrl}
+          alt="no image"
+          style={{
+            backgroundImage: "#000000",
+            height: "100px",
+            weight: "100px",
+          }}
+        />
+        <IconButton onClick={onClearImg}>
+          <ClearRoundedIcon />
+        </IconButton>
       </Box>
-      
       <TextField
         margin="normal"
         type="text"
         required
         fullWidth
         autoFocus
-        name="강의 순서 내용"
-        value="강의 순서 내용"
+        name="lecPeriod"
+        id="lecPeriod"
+        label="Period"
+        value={lecPeriod}
+        onChange={onChange}
       />
-      <AddRoundedIcon/>
-      {/* 이거 그냥 Todo list 처럼 Onchange랑 OnSubmit 이용해서
-      하나씩 추가하고 하나씩 제거하는 걸로 구현 하면 될듯  */}
+
+      <TextField
+        margin="normal"
+        type="text"
+        required
+        fullWidth
+        autoFocus
+        name="lecTime"
+        id="lecTime"
+        label="Time"
+        value={lecTime}
+        onChange={onChange}
+      />
+
       <TextField
         margin="normal"
         type="text"
@@ -253,6 +327,59 @@ const AddLec = () =>{
         value={lecPrice}
         onChange={onChange}
       />
+
+      <TextField
+        margin="normal"
+        type="text"
+        required
+        fullWidth
+        autoFocus
+        name="lecMethod"
+        id="lecMethod"
+        label="Method"
+        value={lecMethod}
+        onChange={onChange}
+      />
+
+      <TextField
+        margin="normal"
+        type="text"
+        required
+        fullWidth
+        autoFocus
+        name="lecLanguage"
+        id="lecLanguage"
+        label="Language"
+        value={lecLanguage}
+        onChange={onChange}
+      />
+
+      {lecIndex.map((h, index) => (
+        <h3>
+          {index + 1}. {h}
+        </h3>
+      ))}
+      <TextField
+        margin="normal"
+        type="text"
+        required
+        fullWidth
+        autoFocus
+        name="index"
+        id="index"
+        label="Index"
+        value={index}
+        onChange={onChange}
+      />
+
+      <IconButton onClick={onAddIndex}>
+        <AddRoundedIcon />
+      </IconButton>
+
+      <IconButton onClick={onDeleteIndex}>
+        <ClearRoundedIcon />
+      </IconButton>
+
       <TextField
         margin="normal"
         type="text"
@@ -281,7 +408,7 @@ const AddLec = () =>{
             name={category}
             key={index}
             variant="contained"
-            color = "settings"
+            color="settings"
             sx={{ m: "10px" }}
             onClick={onClickPush}
           >
